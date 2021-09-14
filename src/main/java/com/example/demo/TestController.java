ac31b5f920f1c422fccfa8c361760bbb803c2a5b
sureshgopalmca <sureshgopalmca@gmail.com>
2021-09-14 21:16:01 +0530

    port path changes

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	
-	@GetMapping("/app")
+	@GetMapping("/")
 	String hello() {
 		return "Welcome to Jenkins";
 	}
